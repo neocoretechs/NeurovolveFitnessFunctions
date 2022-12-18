851fc678b7caa2fe5df9bc24d59d16cf9c84e1b4
NeoCoreTechs - New Core Technologies <groffj@neocoretechs.com>
2022-12-18 10:45:25 -0800

    Update imgclf.java

--- src/imgclf.java
+++ src/imgclf.java
@@ -1,23 +1,16 @@
 
 import static cnn.tools.Util.checkNotEmpty;
 
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
 import java.io.File;
-import java.io.IOException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import com.neocoretechs.neurovolve.Neurosome;
 import com.neocoretechs.neurovolve.NeurosomeInterface;
 import com.neocoretechs.neurovolve.fitnessfunctions.NeurosomeFitnessFunction;
-import com.neocoretechs.neurovolve.properties.LoadProperties;
 import com.neocoretechs.neurovolve.worlds.RockSackWorld;
 import com.neocoretechs.neurovolve.worlds.World;
-
-import com.neocoretechs.relatrix.client.RelatrixClient;
 
 import cnn.components.Plate;
 import cnn.driver.Dataset;
@@ -55,7 +48,7 @@ public class imgclf extends NeurosomeFitnessFunction {
 		}
 	}
 	
-	public static int datasetSize;
+	public static int datasetSize = 0;
 	public static float[][] imageVecs; // each image as 1D float vector
 	private static String[] imageLabels;
 	
@@ -84,14 +77,16 @@ public class imgclf extends NeurosomeFitnessFunction {
 		//if(args.length < 2)
 		//	throw new Exception("Usage:java Infer <LocalIP Client> <Remote IpServer> <DB Port> <GUID of Neurosome> <Image file or directory>");
 		//new RelatrixClient(args[0], args[1], Integer.parseInt(args[2]));
-		Dataset dataset = Util.loadDataset(new File(prefix), null, false);
-		datasetSize = dataset.getSize();
-		System.out.printf("Dataset from %s loaded with %d images%n", prefix, datasetSize);
-		// Construct a new world to spin up remote connection
-		//categoryNames.get(index).getName() is category
-		// MinRawFitness is steps * testPerStep args one and two of setStepFactors
-		((RockSackWorld)world).setStepFactors(datasetSize, 1);
-		createImageVecs(world, dataset);
+		if(datasetSize == 0) {
+			Dataset dataset = Util.loadDataset(new File(prefix), null, false);
+			datasetSize = dataset.getSize();
+			System.out.printf("Dataset from %s loaded with %d images%n", prefix, datasetSize);
+			// Construct a new world to spin up remote connection
+			//categoryNames.get(index).getName() is category
+			// MinRawFitness is steps * testPerStep args one and two of setStepFactors
+			((RockSackWorld)world).setStepFactors(datasetSize, 1);
+			createImageVecs(world, dataset);
+		}
 	}
 	    	
 	@Override
